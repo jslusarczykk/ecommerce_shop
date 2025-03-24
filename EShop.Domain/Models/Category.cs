03ee0b0857b40660dfdfb3312a61242753fc9de7
akalimemecoin <jacgcmain@gmail.com>
2025-03-24 18:12:03 +0100

    i cooked

--- EShop.Domain/Models/Category.cs
+++ EShop.Domain/Models/Category.cs
@@ -1,10 +1,12 @@
 ﻿using static System.Runtime.InteropServices.JavaScript.JSType;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace EShop.Domain.Models
 {
     public class Category : BaseModel
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
     }
